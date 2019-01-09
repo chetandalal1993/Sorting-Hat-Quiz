5e08c14f346b1ba2ac113766332b13ae62fe0c4b
ChelseaTolnai <ChelseaTolnai@gmail.com>
2019-01-09 16:25:29 -0500

    Created Questions and Answer data array.

---
+++ react-sorting-hat/src/api/Questions.js
@@ -0,0 +1,172 @@
+const Questions = [
+    {
+        question: "Before your arrival at Hogwarts you obtained your first wand at Ollivanders. The wand you were destined for has which type of wand core?",
+        answers: [
+            {
+                type: "Hufflepuff",
+                content: "Unicorn Tail Hair"
+            },
+            {
+                type: "Gryffindor",
+                content: "Phoenix Feather"
+            },
+            {
+                type: "Ravenclaw",
+                content: "Dragon Heartstring"
+            },
+            {
+                type: "Slytherin",
+                content: "Horned Serpent Horn"
+            }
+        ]
+    },
+    {
+        question: "Congratulations, you have made it to Hogwarts and you brought along your trusted wizarding pet. Which pet did you bring with you?",
+        answers: [
+            {
+                type: "Hufflepuff",
+                content: "Toad"
+            },
+            {
+                type: "Gryffindor",
+                content: "Owl"
+            },
+            {
+                type: "Ravenclaw",
+                content: "Pygmy Puff"
+            },
+            {
+                type: "Slytherin",
+                content: "Cat"
+            }
+        ]
+    },
+    {
+        question: "How exciting! The first Quidditch match of the year is about to begin and you've made the team you first year. Which Quidditch position are you playing?",
+        answers: [
+            {
+                type: "Hufflepuff",
+                content: "Beater"
+            },
+            {
+                type: "Gryffindor",
+                content: "Keeper"
+            },
+            {
+                type: "Ravenclaw",
+                content: "Chaser"
+            },
+            {
+                type: "Slytherin",
+                content: "Seeker"
+            }
+        ]
+    },
+    {
+        question: "Brilliant! You're team won the Quiddith match with you're help. Alright now, back to the books and time to study up on your favortie subjects, which are ... ",
+        answers: [
+            {
+                type: "Hufflepuff",
+                content: "Herbology & Care of Magical Creatures"
+            },
+            {
+                type: "Gryffindor",
+                content: "Defense Against the Dark Arts & Flying"
+            },
+            {
+                type: "Ravenclaw",
+                content: "Transfiguartion & Divination"
+            },
+            {
+                type: "Slytherin",
+                content: "Potions & Dark Arts"
+            }
+        ]
+    },
+    {
+        question: "While in your History of Magic class you learned about the 'Tale of the Three Brothers' and the Dealthy Hallows. Which character/Hallow do you wish you could have been/received.",
+        answers: [
+            {
+                type: "Hufflepuff",
+                content: "Cadamus Peverell - The Resurrection Stone"
+            },
+            {
+                type: "Gryffindor",
+                content: "Antioch Peverall - The Elder Wand"
+            },
+            {
+                type: "Ravenclaw",
+                content: "Ingnotus Peverell - The Cloak of Invisibility"
+            },
+            {
+                type: "Slytherin",
+                content: "Death itself - The Ultimate Winner"
+            }
+        ]
+    },
+    {
+        question: "Think fast! Someone just casted an unknown spell on you. Which counter spell do you shout in return?",
+        answers: [
+            {
+                type: "Hufflepuff",
+                content: "Protego"
+            },
+            {
+                type: "Gryffindor",
+                content: "Stupefy"
+            },
+            {
+                type: "Ravenclaw",
+                content: "Expelliarmus"
+            },
+            {
+                type: "Slytherin",
+                content: "Crucio!"
+            }
+        ]
+    },
+    {
+        question: "Bonus: The Sorting Hat takes your choice into account. Which Hogwarts house do you wish to be apart of?",
+        answers: [
+            {
+                type: "Hufflepuff",
+                content: "Hufflepuff"
+            },
+            {
+                type: "Gryffindor",
+                content: "Gryffindor"
+            },
+            {
+                type: "Ravenclaw",
+                content: "Ravenclaw"
+            },
+            {
+                type: "Slytherin",
+                content: "Slytherin"
+            }
+        ]
+    },
+    {
+        question: "Just out of curiosity, after taking this quiz which Hogwarts house do you think you will be sorted into?",
+        answers: [
+            {
+                type: "Hufflepuff",
+                content: "Hufflepuff"
+            },
+            {
+                type: "Gryffindor",
+                content: "Gryffindor"
+            },
+            {
+                type: "Ravenclaw",
+                content: "Ravenclaw"
+            },
+            {
+                type: "Slytherin",
+                content: "Slytherin"
+            }
+        ]
+    }
+  ];
+  
+  export default Questions;
