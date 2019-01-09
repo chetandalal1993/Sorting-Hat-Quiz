b980280aa61d1a56331cd2d9d91d774a28b00671
ChelseaTolnai <ChelseaTolnai@gmail.com>
2019-01-09 17:52:24 -0500

    Added detailed notes to handler function and each step processes.

--- react-sorting-hat/src/App.js
+++ react-sorting-hat/src/App.js
@@ -27,73 +27,73 @@ class App extends Component {
   }
 
   componentWillMount = () => {
-    const shuffledAnswerOptions = Questions.map((question) => this.shuffleArray(question.answers));  
+    const shuffledAnswerOptions = Questions.map((question) => this.shuffleArray(question.answers)); // Setting new random order of possible answer array by calling suffle function
 
     this.setState({
-      question: Questions[0].question,
-      answerOptions: shuffledAnswerOptions[0]
+      question: Questions[0].question, // Setting first question -- Displaying first question from Question data array
+      answerOptions: shuffledAnswerOptions[0] // Setting first answers -- Displaying first question's posisble answers randomly from Question data array 
     });
   }
 
-  shuffleArray = (array) => {
-    let currentIndex = array.length, temporaryValue, randomIndex;
+  shuffleArray = (array) => { // Shuffle function being called
+    let currentIndex = array.length, temporaryValue, randomIndex; // Setting current index from answer option length (4) and other variables
 
-    while (0 !== currentIndex) {
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex -= 1;
-      temporaryValue = array[currentIndex];
-      array[currentIndex] = array[randomIndex];
-      array[randomIndex] = temporaryValue;
-    }
+    while (0 !== currentIndex) { // Will continue so long as there are still items in the array. (First index of 4 then --)
+      randomIndex = Math.floor(Math.random() * currentIndex); // Set random new index (ex. 2)
+      currentIndex -= 1; // Current index now 3
+      temporaryValue = array[currentIndex]; // Value set to array[3] or third answer option
+      array[currentIndex] = array[randomIndex]; // array[3] = array[2]
+      array[randomIndex] = temporaryValue; // array[2] = array [3] :: This is essentially swapping to answer option positions using third variable
+    } // Process continues for each index (4 times)
 
-    return array;
-  };
+    return array; // return new random array
+  }
 
-  setUserAnswer = (answer) => {
+  setUserAnswer = (answer) => { // for each selected answer new state to be added
     this.setState((state) => ({
-      answersCount: {
-        ...state.answersCount,
-        [answer]: state.answersCount[answer] + 1
+      answersCount: { // setting new state of answersCount object containing key (house) and values (current score)
+        ...state.answersCount, // utilizing previus state
+        [answer]: state.answersCount[answer] + 1 // taking the key that matches the selected answer and adding 1 to its value
       },
-      answer: answer
+      answer: answer // answer previously empty string now the selected answer
     }));
   }
 
   setNextQuestion = () => {
-    const counter = this.state.counter + 1;
-    const questionId = this.state.questionId + 1;
+    const counter = this.state.counter + 1; // Counter starts at zero and increments by one each question (counter = 0)
+    const questionId = this.state.questionId + 1; // Start at 1 (Question 1) and increments by one (id =1)
     this.setState({
-      counter: counter,
-      questionId: questionId,
-      question: Questions[counter].question,
-      answerOptions: Questions[counter].answers,
-      answer: ''
+      counter: counter, // return new counter (counter = 1)
+      questionId: questionId, // return new id (id = 2)
+      question: Questions[counter].question, // replace last question with next one from Question data array (question = Questions[1].question)
+      answerOptions: Questions[counter].answers, // replace last answer options with next one from Question data array
+      answer: '' // resetting selected answer
     });
   }
 
-  handleAnswerSelected = (e) => {
-    this.setUserAnswer(e.currentTarget.value);
-    if (this.state.questionId < Questions.length) {
-        setTimeout(() => this.setNextQuestion(), 500);
-      } else {
-        setTimeout(() => this.setResults(this.getResults()), 500);
+  handleAnswerSelected = (e) => { // called when answer is selected
+    this.setUserAnswer(e.currentTarget.value); // calling setUserAnswer function with selected answer
+    if (this.state.questionId < Questions.length) { // continue for all items in Question Array
+        setTimeout(() => this.setNextQuestion(), 500); // calling setNextQUestion function after given time
+      } else { // return once all questions answered
+        setTimeout(() => this.setResults(this.getResults()), 500); // calling setResults function after given time using the results from the called getResults function
       }
   }
 
-  getResults() {
-    const answersCount = this.state.answersCount;
-    const answersCountKeys = Object.keys(answersCount);
-    const answersCountValues = answersCountKeys.map((key) => answersCount[key]);
-    const maxAnswerCount = Math.max.apply(null, answersCountValues);
+  getResults = () => { // to determine result
+    const answersCount = this.state.answersCount; // setting answer object of current state (exObj= {H: 5, G: 1, R: 1, S: 1})
+    const answersCountKeys = Object.keys(answersCount); // setting key array from answer object (exArr= [H, G, R, S])
+    const answersCountValues = answersCountKeys.map((key) => answersCount[key]); // iterate over key array and set value array to be the answer object value at that key index (exVals = [5, 1, 1, 1])
+    const maxAnswerCount = Math.max(...answersCountValues); // spread operator to get max number from array (ex. 5)
 
-    return answersCountKeys.filter((key) => answersCount[key] === maxAnswerCount);
+    return answersCountKeys.filter((key) => answersCount[key] === maxAnswerCount); // returns answer object item who has max (exResult. [H] : exObg[H] = 5)
   }
 
-  setResults = (result) => {
-    if (result.length === 1) {
-      this.setState({ result: result[0] });
-    } else {
-      this.setState({ result: 'Undetermined' });
+  setResults = (result) => { // takes in calculated result array (ex. [H])
+    if (result.length === 1) { // if only one max value in array 
+      this.setState({ result: result[0] }); // returns only item (ex. H)
+    } else { // if more than one max value like a tie (ex. [H, G, S])
+      this.setState({ result: result[getRandomInt(result.length)]}); // returns result at random index (ex. length=3 => random index expected 0, 1, or 2)
     }
   }
 
