f289c7a86e5ae175db437c258d6f26893bc15f58
ChelseaTolnai <ChelseaTolnai@gmail.com>
2019-01-09 13:20:10 -0500

    Created initial App page with styling.

--- react-sorting-hat/src/App.js
+++ react-sorting-hat/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -7,12 +6,20 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <img src={require("./designFiles/hogwarts-crest.png")} className="App-logo" alt="Hogwarts Crest" />
+          <h1>Welcome to Hogwarts</h1>
+          <h2>- School of Witchcraft and Wizardry -</h2>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="App-intro">
+          <img src={require("./designFiles/great-hall.gif")} className="App-gif" alt="Hogwart Great Hall" />
+          <p> <span className="lrg-qt">&ldquo; </span>Welcome to Hogwarts. The start-of-term banquet will begin shortly, but before you take your seats in the Great Hall, you will be sorted into your houses. 
+              ... While you are here, your house will be something like your family within Hogwarts. 
+              The four houses are called Gryffindor, Hufflepuff, Ravenclaw, and Slytherin. 
+              Each house has its own noble history and each has produced outstanding witches and wizards. 
+              ... I hope each of you will be a credit to whichever house becomes yours.<span className="lrg-qt"> &rdquo;</span>
+          </p>
+          <div className="App-btn">Be sorted and discover your house!</div>
+        </div>
       </div>
     );
   }
